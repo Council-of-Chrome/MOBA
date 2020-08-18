7c849fad9ebb4033aa109182e86c3c7b8ab36b25
Jade Lotus <elisekainumbers@gmail.com>
2020-08-18 05:14:36 +0100

    Initial Builder Pattern Architecture

---
+++ MOBA-Thing Server/Assets/Scripts/HealthEffector.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public struct HealthEffector
+{
+    public float Value;
+    public Damage_Type Type;
+}
---
+++ MOBA-Thing Server/Assets/Scripts/Minion.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class Minion : ScriptableObject, ITakeDamage, IAutoAttack, IMove, IHasName //TODO: Build entity factory for preparing monobehaviours
+{
+    public string DisplayName { get; }
+
+    public int BaseLevel { get; }
+
+    public float BaseHP { get; }
+    public float HPPerLevel { get; }
+
+    public float MoveSpeed { get; }
+
+    public float AttackDamage { get; }
+    public float AttackPerLevel { get; }
+
+    public float AttackSpeed { get; }
+    public float AttackSpeedPerLevel { get; }
+
+    public float AttackRange { get; }
+    public Range_Class RangeClass { get; }
+}
