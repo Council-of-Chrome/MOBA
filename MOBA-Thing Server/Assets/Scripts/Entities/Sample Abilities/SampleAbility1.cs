6217440f38314820abc6065120fcdab43cd5674d
Jade Lotus <elisekainumbers@gmail.com>
2020-09-02 08:18:50 +0100

    removed useless usings

--- MOBA-Thing Server/Assets/Scripts/Entities/Sample Abilities/SampleAbility1.cs
+++ MOBA-Thing Server/Assets/Scripts/Entities/Sample Abilities/SampleAbility1.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class SampleAbility1 : Ability, IAbilityCastable, ITargetAOE, IAffectHealth
 {
